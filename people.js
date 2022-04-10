b0ed58d5ca00302bcedfd9460aa03d9fb41d7a06
Tamim-hossen <103213985+Tamim-hossen@users.noreply.github.com>
2022-04-10 22:19:56 +0600

    lesson 2

---
+++ people.js
@@ -0,0 +1,13 @@
+//ID: 202-15-14414
+
+const people = ['yoshi', 'ryu', 'chun-li', 'mario'];
+const ages = [20, 25, 30, 35];
+
+// console.log(people);
+
+// module.exports = people;
+
+module.exports = {
+  people,
+  ages,
+}
