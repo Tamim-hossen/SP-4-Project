1a54b52295d6bc24fccc269653a7094411bbe1e9
Tamim-hossen <103213985+Tamim-hossen@users.noreply.github.com>
2022-04-10 22:22:57 +0600

    Lesson 3

---
+++ lesson 3/server.js
@@ -0,0 +1,12 @@
+//ID: 202-15-14414
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('request made');
+});
+
+// localhost is the default value for 2nd argument
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
